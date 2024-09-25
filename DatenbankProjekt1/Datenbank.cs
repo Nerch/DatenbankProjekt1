ac01064478df1f8c5fb53618892c413ab1c79f21
KutznerC <hansfrenzy@gmail.com>
2024-09-25 10:36:44 +0200

    Projektdateien hinzufügen.

---
+++ DatenbankProjekt1/Datenbank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatenbankProjekt1
+{
+    internal class Datenbank
+    {
+        //Eigenschaften der Klasse
+        String Name = "Name der Datenbank.";
+        String Ort = "Pfad zum Ordner wo die Datenbank hinterlegt ist.";
+        String Beschreibung = "Platzhalter für die Beschreibeung.";
+
+
+        public Datenbank() //Konstruktor für die Klasse.
+        {
+            // Konstruktor-Code hier einfügen
+        }
+
+        public void DatabaseConnect() //Methode zum verbinden der Datenbank.
+        {
+            // Code zum Verbinden der Datenbank hier einfügen
+        }
+
+        public void SqliteAbfrage() //Methode um SQL Abfragen zu ermöglichen.
+        {
+            // Code für SQL-Abfragen hier einfügen
+        }
+
+        public string DatabaseAnzeige() //Methode zur Anzeige der Datenbank.
+        {
+            string placeholder = "Platzhalter";
+            Console.WriteLine(placeholder);
+            return placeholder; // Rückgabewert hinzufügen
+        }
+    }
+}
---
+++ DatenbankProjekt1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace DatenbankProjekt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Datenbankeinbindung Test Programm"); 
+
+            Datenbank Testaufruf = new Datenbank(); // Instanzierung der Klasse Datenbank als neues Objekt mit dem Namen Testaufruf.
+            Testaufruf.DatabaseAnzeige(); // Aufruf der Methode der Instanz Testaufruf
+
+        }
+    }
+}
